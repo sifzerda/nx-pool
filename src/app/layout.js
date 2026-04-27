d62ae2e5f4e7c459a6433fc0350b42ca2bff5b3f
Troygit config --global user.email <tydamon@hotmail.com>
2026-04-28 00:43:54 +1000

    footer sits at bottom of screen

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,19 +24,20 @@ export default function RootLayout({ children }) {
 
       <body className="min-h-full bg-[#020308] text-white">
 
-        <div className="relative min-h-screen overflow-hidden">
+        <div className="relative min-h-screen flex flex-col overflow-hidden">
           {/* Outer Border */}
           <div className="pointer-events-none fixed inset-4 z-50 rounded-[36px] border border-cyan-400/20 shadow-[0_0_40px_rgba(34,211,238,0.08),inset_0_0_60px_rgba(34,211,238,0.04)]" />
 
           <Navigation />
 
-            <main className="flex-1">{children}</main>
+          <main className="flex-1">
+            {children}
+          </main>
 
-            <Footer />
- 
+          <Footer />
         </div>
       </body>
-      
+
     </html>
   );
 }
